01603e09f5621b63384983f4e2f927b8ad5ebf6d
Matthew Jarzynowski <matthewjarzy@gmail.com>
2023-07-11 20:44:20 -0400

    Update Lab3.m

--- Lab3.m
+++ Lab3.m
@@ -1,3 +1,4 @@
+%Childsmath Lab 3
 % Problem 1, find the determinant of this vector as a square matrix
 
 x1 = [5 2 2 3 4 3 3 3 2 3 5 5 3 6 4 5 3 1 1 1 4 2 4 6 4 6 1 2 1 5 4 4 5 4 2 4 6 3 4 5 3 5 4 4 1 6 6 4 6 2 4 2 2 3 4 4 6 4 4 5 5 4 2 6 3 2 1 3 4 4 4 4 5 4 2 4 2 4 2 5 2];
